ebe7500df41ab58283c092f792f45f5e59362966
dzikrialifa <dzikrialif99@gmail.com>
2020-01-07 06:00:20 +0700

    Add other functions

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,6 +6,8 @@ class Admin extends CI_Controller {
     public function __construct() 
     { 
         parent::__construct(); 
+        $this->load->library('form_validation');
+        
     }
     
     public function index() 
@@ -16,12 +18,32 @@ class Admin extends CI_Controller {
             'addressLine1','addressLine2','postalCode'
             ,'creditLimit'
         ]);
+        // $this->db->select(['id_role','nama_role']);
         $data['list'] = $this->db->get('customers')->result_array();
         // var_dump($data);die;
+        $this->load->view('template_admin/header');
+        $this->load->view('template_admin/sidebar');
         $this->load->view('admin/index',$data); 
+        $this->load->view('template_admin/footer');
     }
     
-    
+    public function loginadmin()
+    {
+        $this->load->view('auth/loginadmin');
+    }
 
+    public function prosesloginadmin()
+    {
+        $this->form_validation->set_rules('username','Username','trim|required|min_length[5]|max_length[12]');
+        $this->form_validation->set_rules('password','Password','required');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('auth/loginadmin');
+        }else{
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            var_dump($this->input->post());
+        }
+    }
 }
 
