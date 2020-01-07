59e1a891b96fb4dd7e4bce39c09d6048073b56fa
Alfaza Satria <alfazasatria8@gmail.com>
2020-01-07 15:19:58 +0700

    Dashboard done

--- application/views/admin/page/dashboard.php
+++ application/views/admin/page/dashboard.php
@@ -12,11 +12,13 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('assets/admin/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?= base_url('assets/admin/') ?>css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/admin/') ?>css/my.css" rel="stylesheet">
 
 </head>
 
@@ -365,8 +367,8 @@
 
             <!-- Content Row -->
 
-            
-                
+
+
 
             <!-- Content Row -->
             <div class="row">
@@ -403,7 +405,7 @@
                         </div>
                     </div>
 
-                    
+
 
                 </div>
 
