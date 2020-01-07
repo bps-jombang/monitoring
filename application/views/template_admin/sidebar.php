bb6fdc5c69d6476e67a6cdd5150caa1e5400412a
dzikrialifa <dzikrialif99@gmail.com>
2020-01-07 10:49:19 +0700

    Edit sidebar

--- application/views/template_admin/sidebar.php
+++ application/views/template_admin/sidebar.php
@@ -3,10 +3,11 @@
 
       <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-          <i class="fas fa-laugh-wink"></i>
+        <div class="sidebar-brand-icon">
+          <i class="fas fa-chart-bar"></i>
+          <!-- <img src="<?= base_url('assets/admin/img/bps.png') ?>" class="img-thumbnail" alt="Badan Pusat Statistik"> -->
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">BPS Admin <sup>monitoring</sup></div>
       </a>
 
       <!-- Divider -->
@@ -101,13 +102,23 @@
           <span>Tables</span></a>
       </li>
 
+      <!-- Nav Item - Charts -->
+      <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+          <i class="fas fa-fw fa-sign-out-alt"></i>
+          <span>Logout</span></a>
+      </li>
+
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
+      
 
       <!-- Sidebar Toggler (Sidebar) -->
       <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
       </div>
+
+      
 
     </ul>
     <!-- End of Sidebar -->
