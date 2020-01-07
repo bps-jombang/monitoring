f0bba29fc651df7c8c20a5bb301de3b5b470394c
Alfaza Satria <alfazasatria8@gmail.com>
2020-01-07 10:17:46 +0700

    Init

--- assets/admin/js/demo/datatables-demo.js
+++ assets/admin/js/demo/datatables-demo.js
@@ -1,13 +1,17 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function () {
   $('#dataTablesku').DataTable({
-      scrollX: true,
+      "scrollX": true,
       // searching:true,
       // paging: true
-      pagingType: "numbers",
+      // pagingType: "numbers",
       // "lengthMenu": [
       // 	[10, 25, 50, -1],
       // 	[10, 25, 50, "All"]
       // ]
   });
+});
+
+jQuery(document).ready(function() {
+  jQuery(".main-table").clone(true).appendTo('#table-scroll').addClass('clone');
 });
