22334cf90b505f3d2dbf15dec1427eef53e60997
Alfaza Satria <alfazasatria8@gmail.com>
2020-01-08 09:59:18 +0700

    Dashboard layout done

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -16,11 +16,13 @@ class Page extends CI_Controller {
     public function dashboard()
 	{	
         $this->load->view('template_admin/header');
-		$this->load->view('admin/page/dashboard');
+        $this->load->view('template_admin/sidebar');
+        $this->load->view('admin/page/dashboard');
     }
     
     public function profile()
     {   
+        $this->load->view('template_admin/navbar');
         $this->load->view('template_admin/header');
         $this->load->view('template_admin/sidebar');
         $this->load->view('admin/page/profile');
