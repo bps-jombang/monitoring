139dcf9328122d72c5a8e9ab94ddb9cd957ab660
dzikrialifa <dzikrialif99@gmail.com>
2020-01-09 10:30:58 +0700

    Edit controllers admin & views database on table

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -11,18 +11,30 @@ class Admin extends CI_Controller {
     }
     public function tes()
     {
-        // $this->db->select('k.uraian_kegiatan,s.nama_seksi,k.vol,k.satuan,k.target_penyelesaian,u.nama_user,kc.nama_kecamatan,kd.target,kd.realisasi');
-        // $this->db->join('user as u','u.id_user = kd.id_user');
-        // $this->db->join('kegiatan as k','k.id_kegiatan = kd.id_kegiatan');
-        // $this->db->join('kecamatan as kc','kc.id_kecamatan = kd.id_user');
-        // $this->db->join('seksi as s','s.id_seksi = kd.id_user');
+        $this->db->select('k.uraian_kegiatan,s.nama_seksi,k.vol,k.satuan,k.target_penyelesaian,u.nama_user,kc.nama_kecamatan,kd.target,kd.realisasi');
+        $this->db->join('user as u','u.id_user = kd.id_user');
+        $this->db->join('kegiatan as k','k.id_kegiatan = kd.id_kegiatan');
+        $this->db->join('kecamatan as kc','kc.id_kecamatan = kd.id_user');
+        $this->db->join('seksi as s','s.id_seksi = kd.id_user');
         // $this->db->where('u.id_user = kd.id_user');
-        // $data['list'] = $this->db->get('kegiatan_detail as kd')->result_array();
-        $this->db->join('kegiatan_detail' ,'kegiatan_detail.id_user = user.id_user');
-        $data['user']= $this->db->get('user')->result_array();
+        // $this->db->distinct('id_user');
+        $data['list'] = $this->db->get('kegiatan_detail as kd')->result_array();
+        //$this->db->select("id_user");
+        //SELECT COUNT(DISTINCT Country) FROM Customers;
+        // $data['list'] = $this->db->get('kegiatan_detail')->result_array();
+        // var_dump($data['list']);die;
+        // $this->db->join('kegiatan_detail' ,'kegiatan_detail.id_user = user.id_user');
+        // $data['user']= $this->db->get('user')->result_array();
         // $this->db->where('id_user' );
         // $data['list'] = $this->db->get('kegiatan_detail')->result_array();
         // print_r($data['list']);
+        // $this->db->distinct();
+        // $data['distinct'] = $this->db->get('kegiatan_detail')->result_array();
+        
+        // SELECT COUNT(DISTINCT Country) FROM Customers;
+        // $this->db->select(COUNT(DISTINCT id_user));
+        // $this->;
+        echo $this->db->last_query(); 
         $this->load->view('admin/tes',$data); 
     }
     public function index() 
@@ -53,7 +65,14 @@ class Admin extends CI_Controller {
         $this->db->join('seksi as s','s.id_seksi = kd.id_user');
         $data['list'] = $this->db->get('kegiatan_detail as kd')->result_array();
 
-        $data['user']= $this->db->get('user')->result_array();
+        $this->db->order_by('id_kegiatan');
+        $data['orderuraian'] = $this->db->get('kegiatan')->result_array();
+
+        // SELECT * FROM `kegiatan_detail` GROUP BY id_user ASC 
+        $this->db->group_by('id_user');
+        $data['sortuser'] = $this->db->get('kegiatan_detail')->result_array();
+        // $this->db->distinct('id_user');
+        // $data['user']= $this->db->get('user')->result_array();
          // var_dump($data);die;
         $this->load->view('template_admin/header');
         $this->load->view('template_admin/sidebar');
