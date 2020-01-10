835cb033fa9b114ac279a620607bb2dabf3d2d43
dzikrialifa <dzikrialif99@gmail.com>
2020-01-10 15:41:07 +0700

    Edit views on datatables

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,5 +1,3 @@
-    <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column">
 
       <!-- Main Content -->
       <div id="content">
@@ -11,6 +9,18 @@
           <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
             <i class="fa fa-bars"></i>
           </button>
+
+          <!-- Topbar Search -->
+          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            <div class="input-group">
+              <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                  <i class="fas fa-search fa-sm"></i>
+                </button>
+              </div>
+            </div>
+          </form>
 
           <!-- Topbar Navbar -->
           <ul class="navbar-nav ml-auto">
@@ -178,65 +188,63 @@
         <!-- Begin Page Content -->
         <div class="container-fluid">
 
+          <!-- Page Heading -->
+          <h1 class="h3 mb-2 text-gray-800">Tables</h1>
+          <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
+
           <!-- DataTales Example -->
           <div class="card shadow mb-4">
             <div class="card-header py-3">
               <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
             </div>
             <div class="card-body">
-                <div class="row">
-                
-                <div class="col-12 col-lg-12">
-                        <div class="table-responsive">
-                            <table class="table table-bordered display nowrap tabelku" id="dataTableku" width="100%" cellspacing="0">
-                                <thead class="mytable">
-                                  <tr>
-                                  <th rowspan="2" class="align-middle text-left">nomor</th>
-                                  <th rowspan="2" class="align-middle text-center">uraian</th>
-                                  <th rowspan="2" class="align-middle text-center">seksi</th>
-                                  <th rowspan="2" class="align-middle text-center">vol</th>
-                                  <th rowspan="2" class="align-middle text-center">satuan</th>
-                                  <th rowspan="2" class="align-middle text-center">target penyelesaian</th>
+              <div class="table-responsive">
+                  <table class="table table-bordered display nowrap tabelku" id="dataTableku" width="100%" cellspacing="0">
+                      <thead class="mytable">
+                        <tr>
+                        <th rowspan="2" class="text-center">nomor</th>
+                        <th rowspan="2" class="text-center">uraian</th>
+                        <th rowspan="2" class="text-center">seksi</th>
+                        <th rowspan="2" class="text-center">vol</th>
+                        <th rowspan="2" class="text-center">satuan</th>
+                        <th rowspan="2" class="text-center">target penyelesaian</th>
 
-                                  <?php foreach($sortuser as $d) :?>
-                                    <th colspan="2" class="text-center"><?= "nokec" ?> | <?= "nakec"//$d['nama_kecamatan'] ?><br>
-                                    <p class="text-primary"><?= $d['id_user'] ?></p></th>
-                                  <?php endforeach; ?>
-                                    <th rowspan="2" class="align-middle text-center">jml</th>
-                                  </tr>
-                                    <tr>
-                                    <?php foreach($sortuser as $d) :?>
-                                      <th class="target">T</th>
-                                      <th class="realisasi">R</th>
-                                    <?php endforeach; ?></tr>
-                                </thead>
-                                <tbody>
-                                
-                                <?php $no=1; foreach($orderuraian as $d) : ?>
-                                <tr>
-                                    <td><?= $no++; ?></td>
-                                    <td><?= $d['uraian_kegiatan'] ?></td>
-                                    <td><?= $d['id_seksi'] ?></td>
-                                    <td><?= $d['vol'] ?></td>
-                                    <td><?= $d['satuan'] ?></td>
-                                    <td><?= $d['target_penyelesaian'] ?></td>
-                                
-                                      
-                                    <?php foreach($sortuser as $d) :?>
-                                    <td class="kuning"><?= $d['target'] ?></td>
-                                    <td><?= $d['realisasi'] ?></td>
-                                    <?php endforeach;?>
-                                    <td>40</td>
-                                    
-                                </tr>
-                                <?php endforeach;?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+                        <?php foreach($sortuser as $d) :?>
+                          <th colspan="2" class="text-center"><?= "nokec" ?> | <?= "nakec"//$d['nama_kecamatan'] ?><br>
+                          <p class="text-primary"><?= $d['id_user'] ?></p></th>
+                        <?php endforeach; ?>
+                          <th rowspan="2" class="text-center">jml</th>
+                        </tr>
+                          <tr>
+                          <?php foreach($sortuser as $d) :?>
+                            <th class="target">T</th>
+                            <th class="realisasi">R</th>
+                          <?php endforeach; ?></tr>
+                      </thead>
+                      <tbody>
+                      
+                      <?php $no=1; foreach($orderuraian as $d) : ?>
+                      <tr>
+                          <td><?= $no++; ?></td>
+                          <td><?= $d['uraian_kegiatan'] ?></td>
+                          <td><?= $d['id_seksi'] ?></td>
+                          <td><?= $d['vol'] ?></td>
+                          <td><?= $d['satuan'] ?></td>
+                          <td><?= $d['target_penyelesaian'] ?></td>
+                      
+                            
+                          <?php foreach($sortuser as $d) :?>
+                          <td class="kuning"><?= " "//$d['target'] ?></td>
+                          <td><?= " - " //$d['realisasi'] ?></td>
+                          <?php endforeach;?>
+                          <td>40</td>
+                          
+                      </tr>
+                      <?php endforeach;?>
+                      </tbody>
+                  </table>
+              </div>
             </div>
-
           </div>
 
         </div>
@@ -246,9 +254,3 @@
       <!-- End of Main Content -->
 
       
-
-    </div>
-    <!-- End of Content Wrapper -->
-
-  </div>
-  <!-- End of Page Wrapper -->
