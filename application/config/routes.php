ef93e5db823a5576bcf7860a194e49d0c00360ab
dzikrialifa <dzikrialif99@gmail.com>
2020-01-10 15:38:43 +0700

    Edit routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,9 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'admin';
+$route['default_controller'] = 'welcome';
 
-
+$route['admin/tes/(:any)'] = 'admin/tes/$1';
 // ROUTES VIEW LOGIN 
 $route['administrator'] = 'admin/loginadmin';
 
