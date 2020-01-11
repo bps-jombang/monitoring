36f7be99cc70bd00947f7791e933b5a7f4c707cf
dzikrialifa <dzikrialif99@gmail.com>
2020-01-11 20:31:02 +0700

    Edit functions in model to insert data in database

--- application/models/Model_admin.php
+++ application/models/Model_admin.php
@@ -2,24 +2,34 @@
 
 class Model_admin extends CI_Model {  
 
-    private $_table = "seksi", 
-            $user   = "user";
+    private $_seksi = "seksi", $_mitra = "mitra", $_kecamatan = "kecamatan",
+            $_user   = "user", $_kegiatan = "kegiatan";
 
     // ONLY CREATES 
-    public function createSeksi($tabel,$no)
+    public function createData($tabel,$no)
     {
-        $dataMitra = array(
-            'nama_mitra' => ($this->input->post('nama_mitra'))
+        // 1 = seksi, 2 = mitra, 3 kecamatan, 4 = user, 5 = kegiatan
+        
+        $dataMitra = array( // TABEL MITRA
+            'nama_mitra' => ($this->input->post('nama_mitra')) // NAME LABEL DI VIEWS ex: name="apa"
         );
-        $dataKecamatan = array(
+        $dataKecamatan = array( // TABEL KECAMATAN
             'nomor_kecamatan' => stripslashes($this->input->post('nokec')), 
             'nama_kecamatan' => stripslashes($this->input->post('nakec'))
         );
-        // var_dump($data);
-        if ($no == 1) {
+        // SESUAIKAN DI DATABASE YAA
+
+
+        if ($no == 1) { // insert ke tabel seksi
+            $this->db->insert($tabel,$x);
+        }else if($no == 2){ // insert ke tabel mitra
             $this->db->insert($tabel,$dataMitra);
-        }else if($no == 2){
+        }else if($no == 3){ // insert ke tabel kecamatan
             $this->db->insert($tabel,$dataKecamatan);
+        }else if($no == 4){ // insert ke tabel user
+            $this->db->insert($tabel,$x);
+        }else if($no == 5){ // insert tabael kegiatan
+            $this->db->insert($tabel,$x);
         }
     }
     
@@ -28,90 +38,34 @@ class Model_admin extends CI_Model {
     {
         //
     }
-    public function updateKegiatanDanDetail()
-    {
-        //
-    }
-    public function updateSeksi()
-    {
-        //
-    }
-    public function updateJabatan()
-    {
-        //
-    }
-    public function updateMitra()
-    {
-        //
-    }
 
     // ONLY SHOW ALL
-    public function getUser($id = null)
+    public function getUser($status,$id = null)
     {
-        // $res = $id ? $this->db->get_where($this->user,["id_user" => $id])->row_array(); : $this->db->get($this->user)->result_array();
-        if ($id) {
-            return $this->db->get_where($this->user,["id_user" => $id])->row_array();
-        }else{
-            return $this->db->get($this->user)->result_array();
+        // 1 = seksi, 2 = user,3 = mitra, 4 = kegiatan, 5 = kecamatan
+        if ($status == 1) {
+            // select tabel seksi
+            if ($id) {
+                return json_encode($this->db->get_where($this->_seksi,["id_seksi" => $id])->row_array());
+            }else{
+                return json_encode($this->db->get($this->_seksi)->result_array());
+            }
         }
-    }
-    public function getAllKegiatanDanDetail()
-    {
-        //
-    }
-    public function getAllSeksi()
-    {
-        //
-    }
-    public function getAllJabatan()
-    {
-        //
-    }
-    public function getAllMitra()
-    {
-        //
-    }
-
-    // ONLY SHOW BY ID
-    public function getByIdUser($id)
-    {
-        //
-    }
-    public function getByIdKegiatanDanDetail($id)
-    {
-        //
-    }
-    public function getByIdSeksi($id)
-    {
-        //
-    }
-    public function getByIdJabatan($id)
-    {
-        //
-    }
-    public function getByIdMitra($id)
-    {
-        //
+        // else if($status == 2) {
+        //     // select tabel user
+        //     if ($id) {
+        //         return json_encode($this->db->get_where($this->_user,["id_user" => $id])->row_array());
+        //     }else{
+        //         return json_encode($this->db->get($this->_user)->result_array());
+        //     }
+        // }
+        // else{
+        //     return json_encode($this->db->get($this->user)->result_array());
+        // }
     }
 
     // ONLY DELETE
     public function deleteByIdUser()
-    {
-        //
-    }
-    public function deleteByIdKegiatanDanDetail()
-    {
-        //
-    }
-    public function deleteByIdSeksi()
-    {
-        //
-    }
-    public function deleteByIdJabatan()
-    {
-        //
-    }
-    public function deleteByIdMitra()
     {
         //
     }
