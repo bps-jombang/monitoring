3e695b8132ac47623c04b685698823e126c50cfe
dzikrialifa <dzikrialif99@gmail.com>
2020-01-11 12:01:47 +0700

    Edit functions on model admin

--- application/models/Model_admin.php
+++ application/models/Model_admin.php
@@ -2,16 +2,27 @@
 
 class Model_admin extends CI_Model {  
 
-    private  $user           = "user",
-            $seksi          = "seksi",
-            $kegiatan       = "seksi",
-            $kegiatandetail = "seksi",
-            $mitra          = "mitra";
+    private $_table          = "seksi";
+    private $user = "user";
+
 
     // ONLY CREATES 
-    public function createMitra()
+    public function createSeksi($tabel,$no)
     {
         // nama_mitra
+        $dataM = array(
+            'nama_mitra' => $this->input->post('mitra')
+        );
+        $dataKec = array(
+            'nomor_kecamatan' => $this->input->post('nokec'), 
+            'nama_kecamatan' => $this->input->post('nakec')
+        );
+        // var_dump($data);
+        if ($no == 1) {
+            $this->db->insert($tabel,$dataM);
+        }else if($no == 2){
+        $this->db->insert($tabel,$dataKec);
+        }
     }
     
     // ONLY UPDATE
