d90a4a314761cf7aabc3db1594ed0589adc032ec
dzikrialifa <dzikrialif99@gmail.com>
2020-01-12 17:40:09 +0700

    Add models to handle auth

--- application/models/Model_login.php
+++ application/models/Model_login.php
@@ -2,12 +2,16 @@
 
 class Model_login extends CI_Model { 
 
-    public function login($tabel,$where,$username,$password)
+    public function login($tabel,$username,$password)
     {
-        $username   =   stripslashes($username);
-        $password   =   password_verify($password);
+        $username   =   htmlspecialchars($username,true);
+        $password   =   stripslashes($password);
+        $result     =   $this->db->get_where($tabel,['username' => $username])->row_array();
+        if (password_verify($password,$result['password'])) {
+            return $result;
+        }else{
+            return false;
+        }
     }
 
-
 }
-
