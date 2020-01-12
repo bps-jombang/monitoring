f89dcd08d54ccf782a30f3069928fec80905209a
dzikrialifa <dzikrialif99@gmail.com>
2020-01-12 17:42:09 +0700

    Fixed sidebar website

--- application/views/template_admin/sidebar.php
+++ application/views/template_admin/sidebar.php
@@ -1,9 +1,9 @@
 
     <!-- Sidebar -->
-    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <ul class="navku navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('admin'); ?>">
         <div class="sidebar-brand-icon">
           <i class="fas fa-chart-line"></i>
         </div>
@@ -15,7 +15,7 @@
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="<?= base_url('admin'); ?>">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
@@ -70,7 +70,7 @@
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('logout'); ?>">
+        <a class="nav-link" href="<?= base_url('logout'); ?>" data-toggle="modal" data-target="#logoutModal">
           <i class="fas fa-sign-out-alt"></i>
           <span>Logout</span></a>
       </li>
