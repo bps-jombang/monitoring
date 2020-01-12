41de49c729fe89dbf573e5e47f1d97a00c4743da
dzikrialifa <dzikrialif99@gmail.com>
2020-01-12 17:39:36 +0700

    Edit functions to insert data to database

--- application/models/Model_admin.php
+++ application/models/Model_admin.php
@@ -9,7 +9,10 @@ class Model_admin extends CI_Model {
     public function createData($tabel,$no)
     {
         // 1 = seksi, 2 = mitra, 3 kecamatan, 4 = user, 5 = kegiatan, 6 = jabatan
-        
+        $dataSeksi = array(
+            'nama_seksi' => stripslashes($this->input->post('nama_seksi'))
+        );
+
         $dataMitra = array( // TABEL MITRA
             'nama_mitra' => ($this->input->post('nama_mitra')) // NAME LABEL DI VIEWS ex: name="apa"
         );
@@ -17,24 +20,25 @@ class Model_admin extends CI_Model {
             'nomor_kecamatan' => stripslashes($this->input->post('nokec')), 
             'nama_kecamatan' => stripslashes($this->input->post('nakec'))
         );
-        $dataUser = array (
-            'nama_user' => (this->input->post('nama_user'))
+        $dataUser = array ( // USER
+            'nama_user' => stripslashes($this->input->post('nama_user'))
         );
-        $dataKegiatan = array (
-            'uraian_kegiatan' => ($this->input->post('uraian_kegiatan')),
-            'vol' => ($this->input->post('vol')),
-            'satuan' => ($this->input->post('satuan')),
-            'target_penyelesaian' => ($this->input->post('target_penyelesaian'))
+        $dataKegiatan = array ( // KEGIATAN
+            'uraian_kegiatan' => stripslashes($this->input->post('uraian_kegiatan')),
+            'vol' => stripslashes($this->input->post('vol')),
+            'satuan' => stripslashes($this->input->post('satuan')),
+            'target_penyelesaian' => $this->input->post('target_penyelesaian')
         );
 
-        $dataJabatan = array (
-            'nama_jabatan' => ($this->input->post('nama_jabatan'))
+        $dataJabatan = array ( // JABATAN
+            'nama_jabatan' => stripslashes($this->input->post('nama_jabatan'))
         );
         // SESUAIKAN DI DATABASE YAA
 
 
         if ($no == 1) { // insert ke tabel seksi
-            $this->db->insert($tabel,$x);
+            // var_dump($dataSeksi);die;
+            $this->db->insert($tabel,$dataSeksi);
         }else if($no == 2){ // insert ke tabel mitra
             $this->db->insert($tabel,$dataMitra);
         }else if($no == 3){ // insert ke tabel kecamatan
@@ -42,6 +46,7 @@ class Model_admin extends CI_Model {
         }else if($no == 4){ // insert ke tabel user
             $this->db->insert($tabel,$dataUser);
         }else if($no == 5){ // insert tabael kegiatan
+            // var_dump($dataKegiatan);die;
             $this->db->insert($tabel,$dataKegiatan);
         }else if($no == 6){
             $this->db->insert($tabel,$dataJabatan);
@@ -55,18 +60,18 @@ class Model_admin extends CI_Model {
     }
 
     // ONLY SHOW ALL
-    public function getUser($status,$id = null)
+    public function getUser($tabel,$id = null)
     {
         // 1 = seksi, 2 = user,3 = mitra, 4 = kegiatan, 5 = kecamatan
-        if ($status == "seksi") {
+        if ($tabel == 'seksi') {
             // select tabel seksi
             if ($id) {
                 return json_encode($this->db->get_where($this->_seksi,["id_seksi" => $id])->row_array());
             }else{
-                return json_encode($this->db->get($this->_seksi)->result_array());
+                return $this->db->get($this->_seksi)->result_array();
             }
         }
-        else if($status == "user") {
+        else if($tabel == "user") {
             // select tabel user
             if ($id) {
                 return json_encode($this->db->get_where($this->_user,["id_user" => $id])->row_array());
