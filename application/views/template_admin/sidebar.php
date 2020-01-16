8c7302358f88564e0a149e9a8f6b46674d6666b1
dzikrialifa <dzikrialif99@gmail.com>
2020-01-16 15:23:56 +0700

    Edit sidebar menu with different session

--- application/views/template_admin/sidebar.php
+++ application/views/template_admin/sidebar.php
@@ -1,5 +1,6 @@
 
     <!-- Sidebar -->
+    <?php if($this->session->userdata('id_role') == 1) : ?>
     <ul class="navku navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
@@ -17,7 +18,7 @@
       <li class="nav-item active">
         <a class="nav-link" href="<?= base_url('admin'); ?>">
           <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
+          <span>Dashboard Master</span></a>
       </li>
 
       <!-- Divider -->
@@ -55,12 +56,84 @@
         Addons
       </div>
 
-      <!-- Nav Item - Charts -->
+      <!-- Nav Item - Tables -->
       <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span></a>
+        <a class="nav-link" href="<?= base_url('Admin/dataKegiatan') ?>">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Tabel Kegiatan</span></a>
       </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('logout'); ?>" data-toggle="modal" data-target="#logoutModal">
+          <i class="fas fa-sign-out-alt"></i>
+          <span>Logout</span></a>
+      </li>
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+    <?php elseif($this->session->userdata('id_role') == 2) : ?>
+    <ul class="navku navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('admin'); ?>">
+        <div class="sidebar-brand-icon">
+          <i class="fas fa-chart-line"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">BPS ADMIN MONITORING</div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <a class="nav-link" href="<?= base_url('admin'); ?>">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard Admin</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Interface
+      </div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-fw fa-user-plus"></i>
+          <span>Tambah Data</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+
+          <?php 
+          // $i biasa tanpa => ambil value dari key
+          // $i => ambil keynya saja
+          foreach($listmenu as $key => $value) :?>
+          <?php if($key != "Tambah Admin")  { ?>
+              <a class="collapse-item" href="<?= $value ?>"><i class="fas fa-fw fa-user-plus"></i> <?= $key ?></a>
+          <?php } ?>
+          <?php endforeach; ?>
+          </div>
+        </div>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Addons
+      </div>
 
       <!-- Nav Item - Tables -->
       <li class="nav-item">
@@ -83,6 +156,7 @@
       </div>
 
     </ul>
+    <?php endif;?>
     <!-- End of Sidebar -->
 
     <!-- Content Wrapper -->
