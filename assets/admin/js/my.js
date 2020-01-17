061862ed8174dbde18e58d95acab8a9cb3ed858c
dzikrialifa <dzikrialif99@gmail.com>
2020-01-17 22:42:46 +0700

    Add variable to handle routes url

--- assets/admin/js/my.js
+++ assets/admin/js/my.js
@@ -1,13 +1,18 @@
 // $(document).ready(function() {
 	const hapusmitra = "http://localhost/monitoring/hapusmitra";
+	const hapusadmin = "http://localhost/monitoring/hapusadmin";
+	const hapusseksi = "http://localhost/monitoring/hapusseksi";
+	const hapususer = "http://localhost/monitoring/hapususer";
+	const hapusjabatan = "http://localhost/monitoring/hapusjabatan";
 
 
     const tabel = $(".mytable tr th");
     tabel.addClass("align-middle");
 	
-	// tombol hapus
+	// ALERT TAMBAH & UPDATE DATA
 	const flashData = $('.flash-data').data('flashdata');
-	// console.log(flashData);
+	console.log(flashData);
+	
 	if (flashData) {
 		swal.fire({
 			title: 'Data ',
@@ -16,10 +21,11 @@
 		});
 	}
 	
-	// Hapus mitra
-	$('.tombol-hapus').on('click', function(e){
+// ALERT HAPUS DATA
+	// Hapus Mitra
+	$('.tombol-hapus-mitra').on('click', function(e){
 		e.preventDefault();
-		const userid = $(this).attr('id');
+		const mitraid = $(this).attr('id');
 
 		swal.fire({
 			title	: 	'Apakah anda yakin',
@@ -32,7 +38,100 @@
 		}).then((result) => {
 			if (result.value) {
 				$.ajax({
-					url: hapusmitra+'/'+userid,
+					url: hapusmitra+'/'+mitraid,
+					success: function (data) {
+						document.location.reload();
+					}
+				});  
+			}
+		})
+	});
+	// Hapus Admin
+	$('.tombol-hapus-admin').on('click', function(e){
+		e.preventDefault();
+		const adminid = $(this).attr('id');
+
+		swal.fire({
+			title	: 	'Apakah anda yakin',
+			text	: 	'Data ini akan dihapus',
+			icon	:	'warning',
+			showCancelButton	: true,
+			confirmButtonColor	:'#3085d6',
+			cancelButtonColor	:'#d33',
+			confirmButtonText	: 'Ya hapus'
+		}).then((result) => {
+			if (result.value) {
+				$.ajax({
+					url: hapusadmin+'/'+adminid,
+					success: function (data) {
+						document.location.reload();
+					}
+				});  
+			}
+		})
+	});
+	$('.tombol-hapus-seksi').on('click', function(e){
+		e.preventDefault();
+		const idseksi = $(this).attr('id');
+
+		swal.fire({
+			title	: 	'Apakah anda yakin',
+			text	: 	'Data ini akan dihapus',
+			icon	:	'warning',
+			showCancelButton	: true,
+			confirmButtonColor	:'#3085d6',
+			cancelButtonColor	:'#d33',
+			confirmButtonText	: 'Ya hapus'
+		}).then((result) => {
+			if (result.value) {
+				$.ajax({
+					url: hapusseksi+'/'+idseksi,
+					success: function (data) {
+						document.location.reload();
+					}
+				});  
+			}
+		})
+	});
+	$('.tombol-hapus-user').on('click', function(e){
+		e.preventDefault();
+		const iduser = $(this).attr('id');
+
+		swal.fire({
+			title	: 	'Apakah anda yakin',
+			text	: 	'Data ini akan dihapus',
+			icon	:	'warning',
+			showCancelButton	: true,
+			confirmButtonColor	:'#3085d6',
+			cancelButtonColor	:'#d33',
+			confirmButtonText	: 'Ya hapus'
+		}).then((result) => {
+			if (result.value) {
+				$.ajax({
+					url: hapususer+'/'+iduser,
+					success: function (data) {
+						document.location.reload();
+					}
+				});  
+			}
+		})
+	});
+	$('.tombol-hapus-jabatan').on('click', function(e){
+		e.preventDefault();
+		const idjabatan = $(this).attr('id');
+
+		swal.fire({
+			title	: 	'Apakah anda yakin',
+			text	: 	'Data ini akan dihapus',
+			icon	:	'warning',
+			showCancelButton	: true,
+			confirmButtonColor	:'#3085d6',
+			cancelButtonColor	:'#d33',
+			confirmButtonText	: 'Ya hapus'
+		}).then((result) => {
+			if (result.value) {
+				$.ajax({
+					url: hapusjabatan+'/'+idjabatan,
 					success: function (data) {
 						document.location.reload();
 					}
