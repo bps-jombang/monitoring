936c8edda96608a9322c14febf88393cb5d7a19b
dzikrialifa <dzikrialif99@gmail.com>
2020-01-17 11:15:37 +0700

    Add auth and processing update data user

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -6,6 +6,11 @@ class Page extends CI_Controller {
     public function __construct() 
     { 
         parent::__construct(); 
+        $this->load->model(['Model_admin' => 'modeladmin','Model_login' => 'modellogin']);
+        $this->load->helper('admin_helper');
+        if (!$this->session->userdata('username')) {
+            redirect(base_url('loginadmin'));
+        }
     }
     
     public function login()
@@ -30,12 +35,17 @@ class Page extends CI_Controller {
         $this->load->view('template_admin/footer');
     }
     
-    public function profile()
+    public function profile($id)
     {   
-        $this->load->view('template_admin/header');
-        $this->load->view('template_admin/sidebar');
-        $this->load->view('template_admin/navbar');
-        $this->load->view('admin/page/profile');
+        
+        $title['judul']     = "MyProfile | BPS";
+        $data['user']  = $this->db->get('admin',["id_admin" => $id])->row_array();
+        $data['listmenu']   = getMenuLink(); // array di helper   
+
+        $this->load->view('template_admin/header',$title);
+        $this->load->view('template_admin/sidebar',$data);
+        $this->load->view('template_admin/navbar',$data);
+        $this->load->view('admin/page/profile',$data);
         $this->load->view('template_admin/footer');
         
     }
