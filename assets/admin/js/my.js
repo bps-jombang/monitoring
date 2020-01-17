d6b39b3d8eb010d59d9a8f1a1a85edf79ffcceeb
dzikrialifa <dzikrialif99@gmail.com>
2020-01-17 11:12:26 +0700

    Add Ajax,display Sweetalert on all CRUD & Edit datatables

--- assets/admin/js/my.js
+++ assets/admin/js/my.js
@@ -1,41 +1,13 @@
 // $(document).ready(function() {
+	const hapusmitra = "http://localhost/monitoring/hapusmitra";
+
+
     const tabel = $(".mytable tr th");
     tabel.addClass("align-middle");
-    // setTimeout(function () {
-    //     $('#pesan').fadeOut('slow');
-    // }, 500);
-    // $('.btn btn-md btn-default resetyo').click(function () {
-    //     $('.selectpicker').selectpicker('val', 'Mustard');
-	// });
 	
-	
-	// const urlupdatebarang = "http://localhost/admin/editmitra/"
-    // $('#btn_update').on('click', function () {
-    // 	// var idmitra = $('#id_mitra').val();
-    // 	var mitra = $('#nama_mitra').val();
-    // 	// var nabar = $('#nama_barang2').val();
-    // 	// var harga = $('#harga2').val();
-    // 	$.ajax({
-    // 		type: "POST",
-    // 		url: urlupdatebarang,
-    // 		dataType: "JSON",
-    // 		data: {
-	// 			// id_mitra: id_mitra,
-    // 			nama_mitra: mitra
-    // 		},
-    // 		success: function (data) {
-    // 			$('[name="edit_idmitra"]').val("");
-    // 			$('[name="edit_mitra"]').val(mitra);
-    // 			$('#editdata').modal('hide');
-    // 			// tampil_data_barang();
-    // 		}
-    // 	});
-    // 	return false;
-    // });
-
 	// tombol hapus
 	const flashData = $('.flash-data').data('flashdata');
-	console.log(flashData);
+	// console.log(flashData);
 	if (flashData) {
 		swal.fire({
 			title: 'Data ',
@@ -43,21 +15,28 @@
 			icon: 'success'
 		});
 	}
-
+	
+	// Hapus mitra
 	$('.tombol-hapus').on('click', function(e){
 		e.preventDefault();
-		const href = $(this).attr('href');
+		const userid = $(this).attr('id');
+
 		swal.fire({
-			title: 'Apakah anda yakin',
-			text: 'data akan dihapus',
-			icon:'warning',
-			showCancelButton: true,
-			confirmButtonColor:'#3085d6',
-			cancelButtonColor:'#d33',
-			confirmButtonText: 'Ya hapus'
+			title	: 	'Apakah anda yakin',
+			text	: 	'Data ini akan dihapus',
+			icon	:	'warning',
+			showCancelButton	: true,
+			confirmButtonColor	:'#3085d6',
+			cancelButtonColor	:'#d33',
+			confirmButtonText	: 'Ya hapus'
 		}).then((result) => {
 			if (result.value) {
-				document.location.href = href;
+				$.ajax({
+					url: hapusmitra+'/'+userid,
+					success: function (data) {
+						document.location.reload();
+					}
+				});  
 			}
 		})
 	});
