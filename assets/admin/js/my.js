d8c53ef8b97a4769d0e4b1a0d3ad74db47335199
dzikrialifa <dzikrialif99@gmail.com>
2020-01-18 14:14:39 +0700

    Add jQuery events & AJAX to delete data

--- assets/admin/js/my.js
+++ assets/admin/js/my.js
@@ -1,18 +1,21 @@
 // $(document).ready(function() {
-	const hapusmitra = "http://localhost/monitoring/hapusmitra";
-	const hapusadmin = "http://localhost/monitoring/hapusadmin";
-	const hapusseksi = "http://localhost/monitoring/hapusseksi";
-	const hapususer = "http://localhost/monitoring/hapususer";
-	const hapusjabatan = "http://localhost/monitoring/hapusjabatan";
-
+	const hapusadmin 	= "http://localhost/monitoring/hapusadmin";// DONE
+	const hapusmitra 	= "http://localhost/monitoring/hapusmitra"; // DONE
+	const hapusseksi 	= "http://localhost/monitoring/hapusseksi";// DONE
+	const hapususer 	= "http://localhost/monitoring/hapususer";// DONE
+	const hapusjabatan = "http://localhost/monitoring/hapusjabatan"; // DONE
+	const hapuspejabat = "http://localhost/monitoring/hapuspejabat"; // DONE
+	
+	// hapus kegiatan 
+	const hapuskegiatan 		= "http://localhost/monitoring/hapuskegiatan";
+	const hapuskegiatandetail 	= "http://localhost/monitoring/hapuskegiatandetail";
 
     const tabel = $(".mytable tr th");
     tabel.addClass("align-middle");
 	
 	// ALERT TAMBAH & UPDATE DATA
 	const flashData = $('.flash-data').data('flashdata');
-	console.log(flashData);
-	
+	// console.log(flashData);
 	if (flashData) {
 		swal.fire({
 			title: 'Data ',
@@ -70,6 +73,7 @@
 			}
 		})
 	});
+	// Hapus Seksi
 	$('.tombol-hapus-seksi').on('click', function(e){
 		e.preventDefault();
 		const idseksi = $(this).attr('id');
@@ -93,6 +97,7 @@
 			}
 		})
 	});
+	// Hapus User
 	$('.tombol-hapus-user').on('click', function(e){
 		e.preventDefault();
 		const iduser = $(this).attr('id');
@@ -116,6 +121,7 @@
 			}
 		})
 	});
+	// Hapus Jabatan
 	$('.tombol-hapus-jabatan').on('click', function(e){
 		e.preventDefault();
 		const idjabatan = $(this).attr('id');
@@ -139,6 +145,79 @@
 			}
 		})
 	});
-	
+	// Hapus Pejabat
+	$('.tombol-hapus-pejabat').on('click', function(e){
+		e.preventDefault();
+		const idpejabat = $(this).attr('id');
+
+		swal.fire({
+			title	: 	'Apakah anda yakin',
+			text	: 	'Data ini akan dihapus',
+			icon	:	'warning',
+			showCancelButton	: true,
+			confirmButtonColor	:'#3085d6',
+			cancelButtonColor	:'#d33',
+			confirmButtonText	: 'Ya hapus'
+		}).then((result) => {
+			if (result.value) {
+				$.ajax({
+					url: hapuspejabat+'/'+idpejabat,
+					success: function (data) {
+						document.location.reload();
+					}
+				});  
+			}
+		})
+	});
+
+// HAPUS KEGIATAN
+	// Hapus Kegiatan
+	$('.tombol-hapus-kegiatan').on('click', function (e) {
+		e.preventDefault();
+		const idkegiatan = $(this).attr('id');
+
+		swal.fire({
+			title: 'Apakah anda yakin',
+			text: 'Data ini akan dihapus',
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Ya hapus'
+		}).then((result) => {
+			if (result.value) {
+				$.ajax({
+					url: hapuskegiatan + '/' + idkegiatan,
+					success: function (data) {
+						document.location.reload();
+					}
+				});
+			}
+		})
+	});
+	// Hapus Kegiatan detail
+	$('.tombol-hapus-kegiatandetail').on('click', function (e) {
+		e.preventDefault();
+		const idkegdetail = $(this).attr('id');
+
+		swal.fire({
+			title: 'Apakah anda yakin',
+			text: 'Data ini akan dihapus',
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Ya hapus'
+		}).then((result) => {
+			if (result.value) {
+				$.ajax({
+					url: hapuskegiatandetail + '/' + idkegdetail,
+					success: function (data) {
+						document.location.reload();
+					}
+				});
+			}
+		})
+	});
 
 // });
