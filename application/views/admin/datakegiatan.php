d521740f936295a5263f6a2411f592f2be6d9c14
dzikrialifa <dzikrialif99@gmail.com>
2020-01-18 14:17:25 +0700

    Display pejabat at data kegiatan

--- application/views/admin/datakegiatan.php
+++ application/views/admin/datakegiatan.php
@@ -19,45 +19,73 @@
                         <th rowspan="2" class="text-center">satuan</th>
                         <th rowspan="2" class="text-center">target penyelesaian</th>
 
-                        <?php foreach($user as $d) :?>
-                        
-                          <th colspan="2" class="text-center"><?= $d['nomor_kecamatan']; ?> | <?= $d['nama_kecamatan'] ?><br>
-                          <?php if($d['nama_user'] != NULL) : ?>
-                          <p class="text-primary"><a href="<?= base_url('detailkegiatan/') ?><?= $d['id_user']; ?>" class="text-decoration-none"><?= $d['nama_user']; ?></a></p></th>
-                          <?php elseif($d['nama_user'] == NULL) : ?>
+                        <?php // panjang = mengikuti banyaknya user
+                        foreach($listuser as $user) :?>
+                          <th colspan="2" class="text-center"><?= $user['nomor_kecamatan']; ?> | <?= $user['nama_kecamatan'] ?><br>
+                          <?php if($user['nama_user'] != NULL) : ?>
+                          <p class="text-primary"><a href="<?= base_url('detailkegiatan/') ?><?= $user['id_user']; ?>" class="text-decoration-none"><?= $user['nama_user']; ?></a></p></th>
+                          <?php elseif($user['nama_user'] == NULL) : ?>
                           <p class="text-danger"><?= "KOSONG"; ?></p></th>
                           <?php endif;?>
                         <?php endforeach; ?>
+                        
+                        <?php // panjang kesamping = mengikuti banyaknya pejabat
+                        foreach($listpejabat as $pejabat) : ?>
+                            <th colspan="2" class="text-center">
+                            <?php if($pejabat["id_jabatan"] == 1) : ?>
+                            <span class="badge badge-warning"><?= $pejabat['nama_jabatan']; ?></span><?php else : ?><span class="badge badge-info"><?= $pejabat['nama_jabatan']; ?></span> <?php endif;?> <?= $pejabat['nama_seksi'] ?><br><p class="text-primary"><a href="<?= base_url('detailkegiatan/') ?><?= $pejabat['id_pejabat']; ?>" class="text-decoration-none"><?= $pejabat['nama_user']; ?></a></p></th>
+                        <?php endforeach;?>
                           <th rowspan="2" class="text-center">Mitra</th>
                           <th rowspan="2" class="text-center">Jumlah</th>
                           <th rowspan="2" class="text-center">Keterangan</th>
                         </tr>
                           <tr>
-                          <?php foreach($user as $d) :?>
+                          <?php // panjang menurun  target & realisasi = mengikuti banyaknya user
+                          foreach($listuser as $user) :?>
+                            <th class="target">T</th>
+                            <th class="realisasi">R</th>
+                          <?php endforeach; ?>
+                          <?php // panjang menurun  target & realisasi = mengikuti banyaknya user
+                          foreach($listpejabat as $pejabat) :?>
                             <th class="target">T</th>
                             <th class="realisasi">R</th>
                           <?php endforeach; ?></tr>
                       </thead>
-                      <tbody>
                       
-                      <?php $no=1; $nom=1; foreach($orderuraian as $d) : ?>
+                      <tbody>
+                      <!-- isi tabel -->
+                      <?php $no=1; 
+                      // tinggi tabel mengikuti banyaknya kegiatan
+                      foreach($semuakegiatan as $kegiatan) : ?>
                       <tr>
-                          <td><?= $nom++; ?></td>
-                          <td><?= $d['uraian_kegiatan'] ?></td>
-                          <td><?= $d['id_seksi'] ?></td>
-                          <td><?= $d['vol'] ?></td>
-                          <td><?= $d['satuan'] ?></td>
-                          <td><?= $d['target_penyelesaian'] ?></td>
+                          <td><?= $no++; ?></td>
+                          <td><?= $kegiatan['uraian_kegiatan'] ?></td>
+                          <td><?= $kegiatan['id_seksi'] ?></td>
+                          <td><?= $kegiatan['vol'] ?></td>
+                          <td><?= $kegiatan['satuan'] ?></td>
+                          <td><?= $kegiatan['target_penyelesaian'] ?></td>
                       
                             
-                          <?php foreach($user as $ds) :?>
+                          <?php // panjang target & realisasi = mengikuti banyaknya user
+                          foreach($listuser as $user) :?>
                           <td class="kuning">target</td>
                           <td><?= " - " //$d['realisasi'] ?></td>
                           <?php endforeach;?>
-                          <td><?= $no++; ?></td>
+
+                          <?php // target pejabat
+                          foreach($listpejabat as $pejabat) :?>
+                          <td class="kuning">target</td>
+                          <td><?= " - " //$d['realisasi'] ?></td>
+                          <?php endforeach;?>
+                          
+                          <td>
+                          <?php foreach($listmitra as $mitra) : ?>
+                          <?= $mitra['nama_mitra']; ?>
+                          <?php endforeach;?>
+                          </td>
                           
                           <td>jmlh</td>
-                          <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, qui.</td>
+                          <td>tidak ada</td>
                           
                       </tr>
                       <?php endforeach;?>
