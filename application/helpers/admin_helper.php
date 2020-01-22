5e7ef430859bf90316038633d2888c177d7d6489
dzikrialifa <dzikrialif99@gmail.com>
2020-01-22 13:17:46 +0700

    Add helpers to sum,find target & realisasi users

--- application/helpers/admin_helper.php
+++ application/helpers/admin_helper.php
@@ -6,42 +6,95 @@ if ( ! function_exists('random')){
         $CI =& get_instance();
         $query = $CI->db->get('user')->result_array();
         return $query;
+        
     }
 
 }
-if ( ! function_exists('getMenuLink')){
-    function getMenuLink(){
+if ( ! function_exists('target_user'))
+{
+    function target_user($id_kegiatan = null,$id_user = null)
+    {
+        // SELECT id_kegiatan,id_user,target FROM `kegiatan_detail` where id_kegiatan = 1 and id_user =3
+        $CI =& get_instance();
+        $CI->db->select('target');
+        $query = $CI->db->get_where('kegiatan_detail',["id_kegiatan" => $id_kegiatan,"id_user" => $id_user])->row_array();
+        // echo $CI->db->last_query();
+        if ($query == NULL) {
+            return FALSE;
+        }else{
+            return $query;
+        }
+    }
+}
+if ( ! function_exists('realisasi_user'))
+{
+    function realisasi_user($id_kegiatan = null,$id_user = null)
+    {
+        // SELECT id_kegiatan,id_user,realisasi FROM `kegiatan_detail` where id_kegiatan = 1 and id_user =3
+        $CI =& get_instance();
+        $CI->db->select('realisasi');
+        $query = $CI->db->get_where('kegiatan_detail',["id_kegiatan" => $id_kegiatan,"id_user" => $id_user])->row_array();
+        // echo $CI->db->last_query();
+        if ($query == NULL) {
+            return FALSE;
+        }else{
+            return $query;
+        }
+    }
+}
+if ( ! function_exists('total_target'))
+{
+    function total_target($id_kegiatan)
+    {
+        $CI =& get_instance();
+        $CI->db->select('SUM(target)');
+        $query = $CI->db->get_where('kegiatan_detail',["id_kegiatan" => $id_kegiatan])->row_array();
+        // echo $CI->db->last_query();
+        return $query;
+    }
+}
+if ( ! function_exists('total_realisasi'))
+{
+    function total_realisasi($id_kegiatan)
+    {
+        $CI =& get_instance();
+        $CI->db->select('SUM(realisasi)');
+        $query = $CI->db->get_where('kegiatan_detail',["id_kegiatan" => $id_kegiatan])->row_array();
+        // echo $CI->db->last_query();
+        return $query;
+    }
+}
+if ( ! function_exists('getMenuLink'))
+{
+    function getMenuLink()
+    {
         return  [
-                "Tambah Admin"      => base_url('addadmin'),
-                "Tambah Seksi"      => base_url('seksi'),
-                "Tambah Kegiatan"   => base_url('kegiatan'),
-                "Tambah Pejabat"   => base_url('pejabat'),
-                "Tambah Jabatan"    => base_url('jabatan'),
-                "Tambah Target User"=> base_url('targetuser'),
-                "Tambah Anggota"       => base_url('user'),
-                "Tambah Mitra"      => base_url('mitra')
+                "Tambah Admin"          => base_url('addadmin'),
+                "Tambah Seksi"          => base_url('seksi'),
+                "Tambah Kegiatan"       => base_url('kegiatan'),
+                "Tambah Pejabat"        => base_url('pejabat'),
+                "Tambah Jabatan"        => base_url('jabatan'),
+                "Tambah Target User"    => base_url('targetuser'),
+                "Tambah Anggota"        => base_url('user'),
+                "Tambah Mitra"          => base_url('mitra')
         ];
     }
 }
-if ( ! function_exists('getMenuForm')){
-    function getMenuForm(){
+if ( ! function_exists('getMenuForm'))
+{
+    function getMenuForm()
+    {
         return [
-            "Tambah Admin","Tambah Seksi","Tambah Kegiatan","Tambah Pejabat",
-            "Tambah Jabatan","Tambah Target Anggota","Tambah Anggota","Tambah Mitra","Form Tambah Data","Tambah User"
-        
+            "Tambah Admin",
+            "Tambah Seksi",
+            "Tambah Kegiatan",
+            "Tambah Pejabat",
+            "Tambah Jabatan",
+            "Tambah Target Anggota",
+            "Tambah Anggota",
+            "Tambah Mitra",
+            "Form Tambah Data",
+            "Tambah User"
         ];
     }
-}
-if ( ! function_exists('random')){
-   function random(){
-         $number = rand(1111,9999);
-         return $number;
-       }
-   }
- 
-if ( ! function_exists('current_utc_date_time')){
-   function current_utc_date_time(){
-         $dateTime = gmdate("Y-m-d\TH:i:s\Z");;
-         return $dateTime;
-       }
-   }   
+}  
