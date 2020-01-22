c14c724ce109c6c86d866ead830b07f295a92da0
dzikrialifa <dzikrialif99@gmail.com>
2020-01-22 14:46:59 +0700

    Edit functions to update password users

--- application/views/admin/page/profile.php
+++ application/views/admin/page/profile.php
@@ -9,7 +9,8 @@
                                 <hr>
                                 <div class="card-body">
                                     <h5 class="card-title text-dark font-weight-bold"><?= strtoupper($this->session->userdata('username')) ?></h5>
-                                    <p class="card-text text-dark font-weight-bold">Kasie IPDS</p>
+                                    <p class="card-text text-dark font-weight-bold"><span class="badge badge-primary"><?php if($this->session->userdata('id_role') == 1): ?>Superadmin
+                                    <?php else : ?>Admin <?php endif;?></span></p>
                                     <a class="btn btn-primary btn-lg btn-block" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Edit Data</a>
                                 </div>
                             </div>
@@ -17,6 +18,7 @@
                         <div class="col-md-6">
                             <div class="collapse" id="collapseExample">
                                 <div class="card card-body">
+                                <form action="<?= base_url('Page/updatepass/'.$this->session->userdata('id_admin')) ?>" method="post">
                                     <h3 class="font-weight-bold text-dark">Ganti Password</h3>
                                     <div class="form-group">
                                         <label class="font-weight-bold text-dark" for="exampleInputPassword1">Password Lama</label>
@@ -26,7 +28,8 @@
                                         <label class="font-weight-bold text-dark" for="exampleInputPassword1">Password Baru</label>
                                         <input type="password" class="form-control" id="exampleInputPassword1">
                                     </div>
-                                    <a class="btn btn-primary btn-lg btn-block text-white" >Ganti Password</a>
+                                    <button class="btn btn-primary btn-lg btn-block text-white" >Ganti Password</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
