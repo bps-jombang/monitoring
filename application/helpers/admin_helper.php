339d0d3fdc29f7444467fcc53a782e1fd787774d
dzikrialifa <dzikrialif99@gmail.com>
2020-01-22 14:45:30 +0700

    Edit helpers to be simple

--- application/helpers/admin_helper.php
+++ application/helpers/admin_helper.php
@@ -12,13 +12,15 @@ if ( ! function_exists('random')){
 }
 if ( ! function_exists('target_user'))
 {
-    function target_user($id_kegiatan = null,$id_user = null)
+    function target_user($id_kegiatan,$id_user,$id_pejabat)
     {
-        // SELECT id_kegiatan,id_user,target FROM `kegiatan_detail` where id_kegiatan = 1 and id_user =3
         $CI =& get_instance();
         $CI->db->select('target');
-        $query = $CI->db->get_where('kegiatan_detail',["id_kegiatan" => $id_kegiatan,"id_user" => $id_user])->row_array();
-        // echo $CI->db->last_query();
+        if ($id_user != NULL && $id_pejabat == 0) {
+            $query = $CI->db->get_where('kegiatan_detail',["id_kegiatan" => $id_kegiatan,"id_user" => $id_user])->row_array();
+        }elseif($id_pejabat != NULL && $id_user == 0){
+            $query = $CI->db->get_where('kegiatan_detail',["id_kegiatan" => $id_kegiatan,"id_pejabat" => $id_pejabat])->row_array();
+        }
         if ($query == NULL) {
             return FALSE;
         }else{
@@ -28,12 +30,16 @@ if ( ! function_exists('target_user'))
 }
 if ( ! function_exists('realisasi_user'))
 {
-    function realisasi_user($id_kegiatan = null,$id_user = null)
+    function realisasi_user($id_kegiatan,$id_user,$id_pejabat)
     {
         // SELECT id_kegiatan,id_user,realisasi FROM `kegiatan_detail` where id_kegiatan = 1 and id_user =3
         $CI =& get_instance();
         $CI->db->select('realisasi');
-        $query = $CI->db->get_where('kegiatan_detail',["id_kegiatan" => $id_kegiatan,"id_user" => $id_user])->row_array();
+        if ($id_user != NULL && $id_pejabat == 0) {
+            $query = $CI->db->get_where('kegiatan_detail',["id_kegiatan" => $id_kegiatan,"id_user" => $id_user])->row_array();
+        } elseif($id_pejabat != NULL && $id_user == 0) {
+            $query = $CI->db->get_where('kegiatan_detail',["id_kegiatan" => $id_kegiatan,"id_pejabat" => $id_pejabat])->row_array();
+        }
         // echo $CI->db->last_query();
         if ($query == NULL) {
             return FALSE;
