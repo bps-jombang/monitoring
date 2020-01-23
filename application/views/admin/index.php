687bbae160dde064d124437650913d29c44929d4
dzikrialifa <dzikrialif99@gmail.com>
2020-01-23 17:37:18 +0700

    Delete progressbar

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -81,51 +81,5 @@
 
                     <!-- Content Row -->
 
-
-
-
-                    <!-- Content Row -->
-                    <div class="row">
-
-                        <!-- Content Column -->
-                        <div class="col-md-12">
-
-                            <!-- Project Card Example -->
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Realisasi Per Seksi</h6>
-                                </div>
-                                <div class="card-body">
-                                    <h4 class="small font-weight-bold"><?= $listseksi[0]["nama_seksi"]; ?><span class="float-right">10%</span></h4>
-                                    <div class="progress mb-4">
-                                        <div class="progress-bar bg-danger" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <h4 class="small font-weight-bold"><?= $listseksi[1]["nama_seksi"]; ?><span class="float-right">35%</span></h4>
-                                    <div class="progress mb-4">
-                                        <div class="progress-bar bg-warning" role="progressbar" style="width: 35%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <h4 class="small font-weight-bold"><?= $listseksi[2]["nama_seksi"]; ?><span class="float-right">60%</span></h4>
-                                    <div class="progress mb-4">
-                                        <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <h4 class="small font-weight-bold"><?= $listseksi[3]["nama_seksi"]; ?><span class="float-right">45%</span></h4>
-                                    <div class="progress mb-4">
-                                        <div class="progress-bar bg-info" role="progressbar" style="width: 45%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <h4 class="small font-weight-bold"><?= $listseksi[4]["nama_seksi"]; ?><span class="float-right">Complete!</span></h4>
-                                    <div class="progress mb-4">
-                                        <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <h4 class="small font-weight-bold"><?= $listseksi[5]["nama_seksi"]; ?><span class="float-right">80%</span></h4>
-                                    <div class="progress">
-                                        <div class="progress-bar bg-dark" role="progressbar" style="width: 80%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </div>
-
                 </div>
                 <!-- /.container-fluid -->
