94eb068b939e8f103682901b25f1776fbd814608
dzikrialifa <dzikrialif99@gmail.com>
2020-02-02 20:14:36 +0700

    File debug

--- application/views/admin/tes.php
+++ application/views/admin/tes.php
@@ -24,9 +24,9 @@
 
                         <?php // panjang = mengikuti banyaknya user
                         foreach(json_decode($listuser,true) as $user) :?>
-                          <th colspan="2" class="text-center"><?= $user['nomor_kecamatan']; ?> | <?= $user['nama_kecamatan'] ?><br>
+                          <th colspan="2" class="text-center"><?= $user['nomor_kecamatan']; ?> | <?= $user['nama_kecamatan']; ?><br>
                           <?php if($user['nama_user'] != NULL) : ?>
-                          <p class="text-primary"><a href="<?= base_url('detailkegiatan/'.$user['id_user']); ?>" class="text-decoration-none"><?= $user['nama_user']; ?></a></p></th>
+                          <p class="text-primary"><a href="<?= base_url('detailkegiatan/'.$user['id_user'].'/user'); ?>" class="text-decoration-none"><?= $user['nama_user']; ?></a></p></th>
                           <?php elseif($user['nama_user'] == NULL) : ?>
                           <p class="text-danger"><?= "KOSONG"; ?></p></th>
                           <?php endif;?>
@@ -35,9 +35,11 @@
                         foreach(json_decode($listpejabat,true) as $pejabat) : ?>
                             <th colspan="2" class="text-center">
                             <?php if($pejabat["id_jabatan"] == 1) : ?>
-                            <span class="badge badge-warning"><?= $pejabat['nama_jabatan']; ?></span><?php else : ?><span class="badge badge-info"><?= $pejabat['nama_jabatan']; ?></span> <?php endif;?> <?= $pejabat['nama_seksi'] ?><br><p class="text-primary"><a href="<?= base_url('detailkegiatan/'.$pejabat['id_pejabat']) ?>" class="text-decoration-none"><?= $pejabat['nama_user']; ?></a></p></th>
+                            <span class="badge badge-warning"><?= $pejabat['nama_jabatan']; ?></span><?php else : ?><span class="badge badge-info"><?= $pejabat['nama_jabatan']; ?></span> <?php endif;?> <?= $pejabat['nama_seksi']; ?><br><p class="text-primary"><a href="<?= base_url('detailkegiatan/'.$pejabat['id_pejabat'].'/pejabat'); ?>" class="text-decoration-none"><?= $pejabat['nama_user']; ?></a></p></th>
                         <?php endforeach;?>
-                        <th colspan="2"class="text-center">Mitra</th>
+                        <?php foreach(json_decode($listmitra,true) as $mitra):?>
+                        <th colspan="2"class="text-center"><a href="<?= base_url('detailkegiatan/'.$mitra['id_mitra'].'/mitra'); ?>" class="text-decoration-none"><?= $mitra['nama_mitra']; ?></a></th>
+                        <?php endforeach;?>
                           <th colspan="2"class="text-center">Jumlah</th>
                           <th rowspan="2" class="text-center">Keterangan</th>
                         </tr>
@@ -52,8 +54,10 @@
                             <th class="target">T</th>
                             <th class="realisasi">R</th>
                           <?php endforeach; ?>
-                          <th>Tm</th>
-                          <th>R</th>
+                          <?php foreach(json_decode($listmitra,true) as $mitra):?>
+                            <th class="target">T</th>
+                            <th class="realisasi">R</th>
+                          <?php endforeach;?>
 
                           <th>T</th>
                           <th>R</th>
@@ -79,41 +83,54 @@
                             
                            
                            <?php 
-                            $data = target_user($kegiatan['id_kegiatan'],$user['id_user'],0);
+                            $data = target_user($kegiatan['id_kegiatan'],$user['id_user'],0,0);
                             if($data == FALSE ) :?><td class="kuning"></td>
                             <?php else : ?>
-                            <td class="kuning"><?= implode("",target_user($kegiatan['id_kegiatan'],$user['id_user'],0));?></td>
+                            <td class="kuning"><?= implode("",target_user($kegiatan['id_kegiatan'],$user['id_user'],0,0));?></td>
                             <?php endif;?>
                            
                            <?php 
-                           $data = realisasi_user($kegiatan['id_kegiatan'],$user['id_user'],0);
+                           $data = realisasi_user($kegiatan['id_kegiatan'],$user['id_user'],0,0);
                             if($data == FALSE ) :?><td></td>
                             <?php else : ?>
-                            <td><?= implode("",realisasi_user($kegiatan['id_kegiatan'], $user['id_user'],0)); ?></td>
+                            <td><?= implode("",realisasi_user($kegiatan['id_kegiatan'], $user['id_user'],0,0)); ?></td>
                             <?php endif;?>
                             
                           <?php endforeach;?>
                           <?php // target pejabat
                           foreach(json_decode($listpejabat,true) as $pejabat) :?>
                             <?php 
-                              $data = target_user($kegiatan['id_kegiatan'],0,$pejabat['id_pejabat']);
+                              $data = target_user($kegiatan['id_kegiatan'],0,$pejabat['id_pejabat'],0);
                               if($data == FALSE ) :?><td class="kuning"></td>
                               <?php else : ?>
-                              <td class="kuning"><?= implode("",target_user($kegiatan['id_kegiatan'],0,$pejabat['id_pejabat'])); ?></td>
+                              <td class="kuning"><?= implode("",target_user($kegiatan['id_kegiatan'],0,$pejabat['id_pejabat'],0)); ?></td>
                               <?php endif;?>
 
                             <?php  // aman
-                              $data = realisasi_user($kegiatan['id_kegiatan'],0,$pejabat['id_pejabat']);
+                              $data = realisasi_user($kegiatan['id_kegiatan'],0,$pejabat['id_pejabat'],0);
                               if($data == FALSE ) :?><td></td>
                               <?php else : ?>
-                              <td><?= implode("",realisasi_user($kegiatan['id_kegiatan'],0,$pejabat['id_pejabat'])); ?></td>
+                              <td><?= implode("",realisasi_user($kegiatan['id_kegiatan'],0,$pejabat['id_pejabat'],0)); ?></td>
                               <?php endif;?>
                             
                           <?php endforeach;?>
-                          <?php foreach(json_decode($listmitra,true) as $mitra) : ?>
-                          
-                          <td>t</td>
-                          
+
+                          <?php // target mitra
+                          foreach(json_decode($listmitra,true) as $mitra) :?>
+                            <?php 
+                              $data = target_user($kegiatan['id_kegiatan'],0,0,$mitra['id_mitra']);
+                              if($data == FALSE ) :?><td class="kuning"></td>
+                              <?php else : ?>
+                              <td class="kuning"><?= implode("",target_user($kegiatan['id_kegiatan'],0,0,$mitra['id_mitra'])); ?></td>
+                              <?php endif;?>
+
+                            <?php  // aman
+                              $data = realisasi_user($kegiatan['id_kegiatan'],0,0,$mitra['id_mitra']);
+                              if($data == FALSE ) :?><td></td>
+                              <?php else : ?>
+                              <td><?= implode("",realisasi_user($kegiatan['id_kegiatan'],0,0,$mitra['id_mitra'])); ?></td>
+                              <?php endif;?>
+                            
                           <?php endforeach;?>
                           
                           <td><?= implode("",total_target($kegiatan['id_kegiatan'])); ?></td>
