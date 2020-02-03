3e1bce5b256276fb4259d98ce4044ea8515bd877
dzikrialifa <dzikrialif99@gmail.com>
2020-02-03 11:53:16 +0700

    Add time zone Asia Jakarta

--- index.php
+++ index.php
@@ -53,7 +53,14 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+
+/* SET TIMEZONE AREA */
+
+date_default_timezone_set('Asia/Jakarta');
+/*----------------------------*/
+
+
 
 /*
  *---------------------------------------------------------------
